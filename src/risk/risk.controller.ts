d5a509361004b8841f1ca63595bc8e87ae4d1363
Misha Kushka <kushka.misha@gmail.com>
2024-10-26 09:38:08 +0100

    Feat: protocol & chain risk calculation

--- src/risk/risk.controller.ts
+++ src/risk/risk.controller.ts
@@ -5,13 +5,23 @@ import { RiskService } from './risk.service';
 export class RiskController {
   constructor(private readonly riskService: RiskService) {}
 
-  @Get('assets')
-  async getAssetsRisk() {
-    return this.riskService.getAssetsRisk();
+  @Get('chains')
+  async getChainsRisk() {
+    return this.riskService.getChainsRisk();
+  }
+
+  @Get('protocols')
+  async getProtocolsRisk() {
+    return this.riskService.getProtocolsRisk();
   }
 
   @Get('pools')
   async getPoolsRisk() {
     return this.riskService.getPoolsRisk();
+  }
+
+  @Get('assets')
+  async getAssetsRisk() {
+    return this.riskService.getAssetsRisk();
   }
 }
--- src/risk/risk.service.ts
+++ src/risk/risk.service.ts
@@ -1,10 +1,147 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-import { Asset, Pool } from '@prisma/client';
+import { Asset, Chain, Platform, Pool } from '@prisma/client';
 
 @Injectable()
 export class RiskService {
   constructor(private readonly prisma: PrismaService) {}
+
+  ////////////////
+  // CHAIN RISK //
+  ////////////////
+
+  async getChainsRisk() {
+    const chains = await this.prisma.chain.findMany();
+    const maturityRisk = this.maturityRiskChains(chains);
+    console.log({ maturityRisk: maturityRisk.map((mr) => mr.risk) });
+    const marketCapRisk = this.marketCapRiskChains(chains);
+    console.log({ marketCapRisk: marketCapRisk.map((mc) => mc.risk) });
+
+    // Maturity | 0.6
+    // MarketCap | 0.4
+    const weights = {
+      maturity: 0.6,
+      marketCap: 0.4,
+    };
+
+    return maturityRisk.map((mr, i) => ({
+      ...mr,
+      marketCapUSD: mr.marketCapUSD.toString(),
+      risks: {
+        maturity: mr.risk,
+        marketCap: marketCapRisk[i].risk,
+        total:
+          (mr.risk * weights.maturity +
+            marketCapRisk[i].risk * weights.marketCap) /
+          (weights.maturity + weights.marketCap),
+      },
+    }));
+  }
+
+  // 5 | < 1 year
+  // 4 | 1 - 2 years
+  // 3 | 2 - 4 years
+  // 2 | 4 - 6 years
+  // 1 | > 6 years
+  private maturityRiskChains(chains: Chain[]) {
+    const now = new Date().getTime();
+    const msInAYear = 1000 * 60 * 60 * 24 * 365;
+
+    return chains.map((chain) => {
+      if (!chain.dateLaunched) return { ...chain, risk: 5 }; // Default high risk if no date
+
+      const ageInYears = (now - chain.dateLaunched.getTime()) / msInAYear;
+
+      if (ageInYears < 1) return { ...chain, risk: 5 };
+      else if (ageInYears < 2) return { ...chain, risk: 4 };
+      else if (ageInYears < 4) return { ...chain, risk: 3 };
+      else if (ageInYears < 6) return { ...chain, risk: 2 };
+      else return { ...chain, risk: 1 };
+    });
+  }
+
+  // 5 | < 500M
+  // 4 | 500M - 2B
+  // 3 | 2B - 10B
+  // 2 | 10B - 50B
+  // 1 | > 50B
+  private marketCapRiskChains(chains: Chain[]) {
+    return chains.map((chain) => {
+      if (chain.marketCapUSD < 500000000) return { ...chain, risk: 5 };
+      else if (chain.marketCapUSD < 2000000000) return { ...chain, risk: 4 };
+      else if (chain.marketCapUSD < 10000000000) return { ...chain, risk: 3 };
+      else if (chain.marketCapUSD < 50000000000) return { ...chain, risk: 2 };
+      else return { ...chain, risk: 1 };
+    });
+  }
+
+  ///////////////////
+  // PROTOCOL RISK //
+  ///////////////////
+
+  async getProtocolsRisk() {
+    const protocols = await this.prisma.platform.findMany();
+    const tvlRisk = this.tvlRiskProtocols(protocols);
+    console.log({ tvlRisk: tvlRisk.map((tr) => tr.risk) });
+    const maturityRisk = this.maturityRiskProtocols(protocols);
+    console.log({ maturityRisk: maturityRisk.map((mr) => mr.risk) });
+
+    // Maturity | 0.4
+    // TVL      | 0.6
+    const weights = {
+      tvl: 0.6,
+      maturity: 0.4,
+    };
+
+    return maturityRisk.map((mr, i) => ({
+      ...mr,
+      tvlUSD: mr.tvlUSD.toString(),
+      risks: {
+        tvl: tvlRisk[i].risk,
+        maturity: mr.risk,
+        total:
+          (tvlRisk[i].risk * weights.tvl + mr.risk * weights.maturity) /
+          (weights.tvl + weights.maturity),
+      },
+    }));
+  }
+
+  // 5 | < 10M
+  // 4 | 10M - 100M
+  // 3 | 100M - 500M
+  // 2 | 500M - 1B
+  // 1 | > 1B
+  private tvlRiskProtocols(protocols: Platform[]) {
+    return protocols.map((protocol) => {
+      if (protocol.tvlUSD < 10000000) return { ...protocol, risk: 5 };
+      else if (protocol.tvlUSD < 100000000) return { ...protocol, risk: 4 };
+      else if (protocol.tvlUSD < 500000000) return { ...protocol, risk: 3 };
+      else if (protocol.tvlUSD < 1000000000) return { ...protocol, risk: 2 };
+      else return { ...protocol, risk: 1 };
+    });
+  }
+
+  // 5 | < 6 months
+  // 4 | 6 months - 1 year
+  // 3 | 1 year - 2 years
+  // 2 | 2 years - 3 years
+  // 1 | > 3 years
+  private maturityRiskProtocols(protocols: Platform[]) {
+    const now = new Date().getTime();
+    const msInAMonth = 1000 * 60 * 60 * 24 * 30;
+
+    return protocols.map((protocol) => {
+      if (!protocol.dateLaunched) return { ...protocol, risk: 5 }; // Default high risk if no date
+
+      const ageInMonths = (now - protocol.dateLaunched.getTime()) / msInAMonth;
+
+      if (ageInMonths < 6) return { ...protocol, risk: 5 };
+      else if (ageInMonths < 12) return { ...protocol, risk: 4 };
+      else if (ageInMonths < 24) return { ...protocol, risk: 3 };
+      else if (ageInMonths < 36) return { ...protocol, risk: 2 };
+      else return { ...protocol, risk: 1 };
+    });
+  }
 
   ///////////////
   // POOL RISK //
@@ -17,9 +154,11 @@ export class RiskService {
     const maturityRisk = this.maturityRiskPools(pools);
     console.log({ maturityRisk: maturityRisk.map((mr) => mr.risk) });
 
+    // Maturity | 0.3
+    // TVL      | 0.7
     const weights = {
-      tvl: 0.4,
-      maturity: 0.6,
+      tvl: 0.7,
+      maturity: 0.3,
     };
 
     return maturityRisk.map((mr, i) => ({
@@ -78,9 +217,9 @@ export class RiskService {
 
   async getAssetsRisk() {
     const assets = await this.prisma.asset.findMany();
-    const maturityRisk = this.maturityRisk(assets);
+    const maturityRisk = this.maturityRiskAssets(assets);
     console.log({ maturityRisk: maturityRisk.map((mr) => mr.risk) });
-    const marketCapRisk = this.marketCapRisk(assets);
+    const marketCapRisk = this.marketCapRiskAssets(assets);
     console.log({ marketCapRisk: marketCapRisk.map((mc) => mc.risk) });
 
     const weights = {
@@ -108,7 +247,7 @@ export class RiskService {
   // 3 | 2 - 3 years
   // 2 | 3 - 5 years
   // 1 | > 5 years
-  private maturityRisk(assets: Asset[]) {
+  private maturityRiskAssets(assets: Asset[]) {
     const now = new Date().getTime();
     const msInAYear = 1000 * 60 * 60 * 24 * 365;
 
@@ -130,7 +269,7 @@ export class RiskService {
   // 3 | 500M - 1B
   // 2 | 1B - 10B
   // 1 | > 10B
-  private marketCapRisk(assets: Asset[]) {
+  private marketCapRiskAssets(assets: Asset[]) {
     return assets.map((asset) => {
       if (asset.marketCapUSD < 50000000) return { ...asset, risk: 5 };
       else if (asset.marketCapUSD < 500000000) return { ...asset, risk: 4 };
